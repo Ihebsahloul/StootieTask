8b15658cd9f33eb68e4485b2730892049ee7319d
Iheb SAHLOUL <ihebsahloul@hotmail.fr>
2020-09-08 19:03:06 +0100

    implement allcountries observer

---
+++ app/src/main/java/com/stootie/task/countries/CountriesListObserver.kt
@@ -0,0 +1,14 @@
+package com.stootie.task.countries
+
+import com.stootie.domain.countries.model.Country
+import io.reactivex.observers.DisposableSingleObserver
+
+class CountriesListObserver(private val presenter: CountriesPresenter): DisposableSingleObserver<List<Country>>() {
+  override fun onSuccess(articlesList: List<Country>) {
+    presenter.showCountriesList(articlesList)
+  }
+
+  override fun onError(e: Throwable) {
+    e.printStackTrace()
+  }
+}
